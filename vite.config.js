1f2b50d1ba329a216169b9dd672cb977b59bbcfd
Ansel <238927804+Ansel-S@users.noreply.github.com>
2026-03-20 13:26:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,15 @@ import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  plugins: [viteSingleFile()],
-  build: {
-    target: "esnext",
-    assetsInlineLimit: 100000000,
-    minify: "terser", 
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
+    plugins: [viteSingleFile()],
+    build: {
+        target: "esnext",
+        assetsInlineLimit: 100000000,
+        minify: "terser", 
+        terserOptions: {
+            compress: {
+                drop_console: true,
+            },
+        },
     },
-  },
 });
